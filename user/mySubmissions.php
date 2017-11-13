8654875de38517fe318797921ffeb8bfb46150f7
Tanvir Ahmed <tanvirahmed14@gmail.com>
2017-11-13 18:59:23 +0600

    HTML table for submission

--- user/mySubmissions.php
+++ user/mySubmissions.php
@@ -2,44 +2,31 @@
 <html>
 <center><h1><strong>My Submissions</strong></h1></center>
 <center><a href="index.php"><b><h2>Home</h2></a></b></center>
+
+<style>
+th, td {
+    border: 1px solid black;
+    padding:  5px 75px 5px 75px;
+}
+</style>
+
 <?php
      $name = $_SESSION['userName'];
      include('connectdb.php');
-
-	 echo "<h2>ID";
-		echo str_repeat('&nbsp;', 10);
-		echo "When";
-		echo  str_repeat('&nbsp;', 12);
-		echo "Who";
-		echo  str_repeat('&nbsp;', 10);
-		echo "Problem";
-		echo  str_repeat('&nbsp;', 10);
-		echo "Language";
-		echo  str_repeat('&nbsp;', 10);        
-		echo "Verdict <br></h2>";
 
 		$query = "select * from submitteddata where userName = '$name' order by ID desc";
 		$result = mysql_query($query, $link) or die("Cannot Retrive From Table");
 
 		$num_rows = mysql_num_rows($result);
+		echo "<table><tr><th>ID</th><th>When</th><th>Who</th><th>Problem</th><th>Language</th><th>Verdict</th></tr>";
 
 		while ($num_rows >0) {
 			$row = mysql_fetch_row($result);
-			echo "<h4>";
-			echo $row['0'];
-			echo str_repeat('&nbsp;', 10);
-			echo $row['1'];
-			echo str_repeat('&nbsp;', 10);
-			echo $row['2'];
-			echo str_repeat('&nbsp;', 15);
-			echo $row['3'];
-			echo str_repeat('&nbsp;', 20);
-			echo $row['4'];
-			echo str_repeat('&nbsp;', 40);
-			echo $row['5']."<br>";
-			echo "</h4>";
+			echo "<tr><td>".$row['0']."</td><td>".$row['1']."</td><td>".$row['2']."</td><td>".$row['3']."</td><td>".$row['4']."</td><td>".$row['5']."</td></tr>";
 			$num_rows--;
+
 		}
+		echo "</table>";
 		mysql_close();
 
 ?>
